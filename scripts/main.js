87ba75b1a77ae2b0c3102f85a13de8c55ddbf544
midridge <72268381+midridge@users.noreply.github.com>
2022-03-06 20:45:36 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/Mozilla_Firefox_logo.png') {
-        myImage.setAttribute('src', 'images/Chrome_logo.png');
-    } else {
-        myImage.setAttribute('src', 'images/Mozilla_Firefox_logo.png')
+        myImage.setAttribute('src', 'images/20220306194600.jpg');
     }
 }
 
@@ -13,12 +11,12 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-    let myName = prompt('Please enter your name.');
+    let myName = prompt('输入你的名字.');
     if(!myName) {
         setUserName();
     } else {
         localStorage.setItem('name', myName);
-        myHeading.textContent = 'Mozilla is cool, ' + myName;
+        myHeading.textContent = myName + '，点击图片了解你的下一个对象';
     }
 }
 
